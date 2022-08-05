a67f10b780c0c7c8f504e3bde836cefde9a26e8c
Alex <al.chumakov96@gmail.com>
2022-08-06 00:54:29 +0800

    heroku deployment

--- core/settings.py
+++ core/settings.py
@@ -114,9 +114,11 @@ USE_TZ = True
 
 
 # Static (CSS, JavaScript, Images)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
 # Media
