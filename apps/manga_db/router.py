6845eec57c3ce3e0425075716aa23c28b04d385e
Alex <al.chumakov96@gmail.com>
2022-09-14 16:20:09 +0800

    trailing slash fix

--- apps/manga_db/router.py
+++ apps/manga_db/router.py
@@ -4,10 +4,18 @@ from .views import (
     MangaReviewsView
 )
 
-get_manga_list = MangaView.as_view({"get": "list"})
+get_manga_list = MangaView.as_view({
+    "get": "list"
+})
 
-get_manga_detail = MangaView.as_view({"get": "retrieve"})
+get_manga_detail = MangaView.as_view({
+    "get": "retrieve"
+})
 
-get_manga_comments_list = MangaCommentsView.as_view({"get": "list"})
+get_manga_comments_list = MangaCommentsView.as_view({
+    "get": "list"
+})
 
-get_manga_reviews_list = MangaReviewsView.as_view({"get": "list"})
+get_manga_reviews_list = MangaReviewsView.as_view({
+    "get": "list"
+})
--- apps/manga_db/urls.py
+++ apps/manga_db/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path("manga/<int:id>/", get_manga_detail),
     path("manga/<int:id>/comments/", get_manga_comments_list),
     path("manga/<int:id>/reviews/", get_manga_reviews_list),
-    path("manga/<int:id>/favorites", MangaFavoritesView.as_view()),
+    path("manga/<int:id>/favorites/", MangaFavoritesView.as_view()),
 ]
